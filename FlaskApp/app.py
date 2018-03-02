db742d464e1f81203222887d06fdaea8eac6923b
bendoan4366 <ben.doan4366@gmail.com>
2018-03-02 12:14:43 -0500

    "sign up with Ajax and JS

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, json, request
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+
+mysql = MySQL()
+ 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'jay'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route('/')
 def main():
@@ -10,7 +21,7 @@ def main():
 def showSignUp():
     return render_template('signup.html')	
 
-@app.route('/signUp',methods=['POST'])
+@app.route('/signUp', methods=['POST'])
 def signUp():
 
 	 # read the posted values from the UI
@@ -20,7 +31,21 @@ def signUp():
 
       # validate the received values
     if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields good !!</span>'})
+        
+        # All Good, let's call MySQL
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+            
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+    			conn.commit()
+    			return json.dumps({'message':'User created successfully !'})
+			else:
+    			return json.dumps({'error':str(data[0])})
     else:
         return json.dumps({'html':'<span>Enter the required fields</span>'})
 
